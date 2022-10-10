0806feb8d785c4d533e7a85124cb26291b1c7067
SanginiMiharia <500094896@stu.upes.ac.in>
2022-10-10 12:13:04 +0530

    Firstpart

---
+++ firstpart.java
@@ -0,0 +1,7 @@
+class SwingCalculator {
+    public static void main(String[] args) {
+	        JFrame frame = new Calculator();
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setVisible(true);
+	    }
+	}
